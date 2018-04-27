362c34902115673c64bca6291e68706bad5d4b26
William Jockusch <jockusch@gmail.com>
2018-04-27 14:20:08 -0500

    A passing test, a failing test, and a skipped test

---
+++ TestTesting/TestTesting/Tests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xunit;
+
+namespace TestTesting
+{
+    public class Tests
+    {
+        [Fact]
+        public void Passing()
+        {
+            Assert.True(true);
+        }
+        [Fact]
+        public void Failing()
+        {
+            Assert.True(false);
+        }
+        [Fact(Skip ="Skipping")]
+        public void Skipped()
+        {
+            Assert.True(false);
+        }
+    }
+}
